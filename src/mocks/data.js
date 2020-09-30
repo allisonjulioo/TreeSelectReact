f790420a2890c0abe64d9b4c7b3c7d5c1c1c04d0
Allison Nunes <allison.julio@hotmail.com>
2020-09-30 00:42:59 -0300

    create mocks to test

---
+++ src/mocks/data.js
@@ -0,0 +1,36 @@
+export default {
+  0: {
+    id: '2469bdab-23b5-4cb8-90c9-c609a49410b0',
+    name: 'Richard Paul M.',
+    children: {
+      0: {
+        id: '97cd3a19-0f1c-4248-a84c-a1f5a0093a89',
+        name: 'Luis F. Doris',
+        children: {
+          0: {
+            id: '6410eff5-5aff-46fd-bb08-ed90581007b4',
+            name: 'Maurice Rudolf Ludwig',
+            children: {
+              0: {
+                id: '35996ee4-74a6-4343-ba5e-9700c24bee11',
+                name: 'Joseph E. James A.',
+                children: {
+                  0: {
+                    id: '59fdebaf-0229-4d27-901d-4cfbb4cf81de',
+                    name: 'Alan G. William',
+                    children: {},
+                    level: 4,
+                  },
+                },
+                level: 3,
+              },
+            },
+            level: 2,
+          },
+        },
+        level: 1,
+      },
+    },
+    level: 0,
+  },
+};
---
+++ src/mocks/functions.js
@@ -0,0 +1,3 @@
+const converteObjArr = jest.fn((node) => []);
+
+ 
